b1607baaaa60d4c97c9d3b348f9575b1a08aa644
Julijinimo <ownflame@gmail.com>
2022-09-08 14:51:45 +0300

    task 9 in progress

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,16 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+function Movie() {
+    this.title = '';
+    this.director = '';
+    this.budget = 'budget';
+};
+
+const Movie1 = new Movie('Antanas', 'Antano brolis', 13);
+
+console.log(Movie1);
+
+//https://rollbar.com/blog/javascript-constructors/
+//https://www.w3schools.com/js/js_object_constructors.asp
