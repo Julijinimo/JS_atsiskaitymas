03f6b9f6297cccfa1530f207805d52c48bca3a8b
Julijinimo <ownflame@gmail.com>
2022-09-08 23:45:20 +0300

    task 8 edit

--- Task 8/script.js
+++ Task 8/script.js
@@ -6,19 +6,38 @@ multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
-function calculate(a, b, operator) {
-    switch(operator){
-      case 'sum':
-        return a + b;
-      case 'subtraction':
-        return a - b;
-      case 'division':
-        return a / b;
-      case 'multiplication':
-        return a * b;
-    };
-  };
-  console.log(calculate(5, 10, 'sum'));
-  console.log(calculate(6, 4, 'subtraction'));
-  console.log(calculate(7, 8, 'multiplication'));
-  console.log(calculate(2, 10, 'division'));
+// function calculate(a, b, operator) {
+//     switch(operator){
+//       case 'sum':
+//         return a + b;
+//       case 'subtraction':
+//         return a - b;
+//       case 'division':
+//         return a / b;
+//       case 'multiplication':
+//         return a * b;
+//     };
+//   };
+//   console.log(calculate(5, 10, 'sum'));
+//   console.log(calculate(6, 4, 'subtraction'));
+//   console.log(calculate(7, 8, 'multiplication'));
+//   console.log(calculate(2, 10, 'division'));
+
+
+  function Calculator(sum, subtraction, multiplication, division) {               // constructor funkcija
+    this.sum = sum;
+    this.b = b;
+    this.sum = function() {
+      return a + b
+    }
+    this.subtraction = function() {
+      return a - b
+    }
+    this.multiplication = function() {
+      return a * b
+    }
+    this.division = function() {
+      return a / b
+    }};
+
+console.log(sum());
