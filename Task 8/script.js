4eb5d21c98a6ff8055029a9cb546b6c529ff8a25
Julijinimo <ownflame@gmail.com>
2022-09-08 14:29:16 +0300

    task 8 complete

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,20 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function calculate(a, b, operator) {
+    switch(operator){
+      case 'sum':
+        return a + b;
+      case 'subtraction':
+        return a - b;
+      case 'division':
+        return a / b;
+      case 'multiplication':
+        return a * b;
+    };
+  };
+  console.log(calculate(5, 10, 'sum'));
+  console.log(calculate(6, 4, 'subtraction'));
+  console.log(calculate(7, 8, 'multiplication'));
+  console.log(calculate(2, 10, 'division'));
