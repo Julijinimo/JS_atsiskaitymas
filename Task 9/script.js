08055cc6c7dbbabd714e8a246bc4828fb67657c1
Julijinimo <ownflame@gmail.com>
2022-09-08 21:54:07 +0300

    Task 9 complete

--- Task 9/script.js
+++ Task 9/script.js
@@ -10,15 +10,25 @@ Metodas:
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
 
-function Movie() {
-    this.title = '';
-    this.director = '';
-    this.budget = 'budget';
-};
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
 
-const Movie1 = new Movie('Antanas', 'Antano brolis', 13);
+    wasExpensive() {
+        return this.budget >= 100000000 
+    }
+}
 
-console.log(Movie1);
+const movie1 = new Movie('Harry Potter', 'Chris Columbus', 100000000);
+const movie2 = new Movie('The Fast and the Furious', 'Vin Diesel', 150000000);
+const movie3 = new Movie('The Godfather', 'Francis Ford Coppola', 90000000);
+console.log(movie1);
+console.log(movie2);
+console.log(movie3);    
 
-//https://rollbar.com/blog/javascript-constructors/
-//https://www.w3schools.com/js/js_object_constructors.asp
+console.log(movie1.wasExpensive());
+console.log(movie2.wasExpensive());
+console.log(movie3.wasExpensive());
