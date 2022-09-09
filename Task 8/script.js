f45b7a5d853b590d4ebca9462221defd12a972ea
Julijinimo <ownflame@gmail.com>
2022-09-09 12:34:23 +0300

    Task 8 updated, tikiuosi dabar bus gerai

--- Task 8/script.js
+++ Task 8/script.js
@@ -6,19 +6,27 @@ multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
-function calculate(a, b, operator) {
-    switch(operator){
-      case 'sum':
-        return a + b;
-      case 'subtraction':
-        return a - b;
-      case 'division':
-        return a / b;
-      case 'multiplication':
-        return a * b;
-    };
-  };
-  console.log(calculate(5, 10, 'sum'));
-  console.log(calculate(6, 4, 'subtraction'));
-  console.log(calculate(7, 8, 'multiplication'));
-  console.log(calculate(2, 10, 'division'));
+function Calculator(a, b) {              
+    this.sum = function() {
+        return (a + b);
+    }
+    this.subtraction = function() {
+      return (a - b);
+    }
+    this.multiplication = function() {
+      return (a * b);
+    }
+    this.division = function() {
+      return (a / b);
+    }
+}
+
+const sum = new Calculator(1, 2);
+const subtraction = new Calculator(2, 2);
+const multiplication = new Calculator(2, 2);
+const division = new Calculator(2, 10);
+
+console.log(sum.sum());
+console.log(subtraction.subtraction());
+console.log(multiplication.multiplication());
+console.log(division.division());
